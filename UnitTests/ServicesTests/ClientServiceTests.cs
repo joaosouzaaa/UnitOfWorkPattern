fbfb6b838366395f88652702d022543db1b30f8d
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-13 23:09:14 -0300

    TestsL ClientService tests

--- UnitTests/ServicesTests/ClientServiceTests.cs
+++ UnitTests/ServicesTests/ClientServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using Moq;
+using UnitOfWorkPattern.API.DataTransferObjects.Client;
+using UnitOfWorkPattern.API.Entities;
 using UnitOfWorkPattern.API.Interfaces.Repositories;
 using UnitOfWorkPattern.API.Interfaces.Settings;
 using UnitOfWorkPattern.API.Services;
@@ -17,5 +19,82 @@ public sealed class ClientServiceTests
         _logRepositoryMock = new Mock<ILogRepository>();
         _notificationHandlerMock = new Mock<INotificationHandler>();
         _clientService = new ClientService(_clientRepositoryMock.Object, _logRepositoryMock.Object, _notificationHandlerMock.Object);
+    }
+
+    [Fact]
+    public async Task AddAsync_SuccessfulScenario_CallAdd()
+    {
+        // A
+        var clientSave = new ClientSave()
+        {
+            Age = 12,
+            Balance = 20,
+            Name = "Test"
+        };
+
+        _clientRepositoryMock.Setup(c => c.AddAsync(It.IsAny<Client>()));
+        _logRepositoryMock.Setup(c => c.AddAsync(It.IsAny<Log>()));
+
+        // A
+        await _clientService.AddAsync(clientSave);
+
+        // A
+        _notificationHandlerMock.Verify(n => n.AddNotification(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+        _clientRepositoryMock.Verify(c => c.AddAsync(It.IsAny<Client>()), Times.Once());
+        _logRepositoryMock.Verify(c => c.AddAsync(It.IsAny<Log>()), Times.Once());
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidClientParameters))]
+    public async Task AddAsync_EntityInvalid_DoNotCallAdd(ClientSave clientSave)
+    {
+        await _clientService.AddAsync(clientSave);
+
+        _notificationHandlerMock.Verify(n => n.AddNotification(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+        _clientRepositoryMock.Verify(c => c.AddAsync(It.IsAny<Client>()), Times.Never());
+        _logRepositoryMock.Verify(c => c.AddAsync(It.IsAny<Log>()), Times.Never());
+    }
+
+    public static IEnumerable<object[]> InvalidClientParameters()
+    {
+        yield return new object[]
+        {
+            new ClientSave()
+            {
+                Age = 0,
+                Balance = 1,
+                Name = "Test"
+            }
+        };
+
+        yield return new object[]
+        {
+            new ClientSave()
+            {
+                Age = -20,
+                Balance = 1,
+                Name = "Test"
+            }
+        };
+
+        yield return new object[]
+        {
+            new ClientSave()
+            {
+                Age = 10,
+                Balance = 1,
+                Name = ""
+            }
+        };
+
+        yield return new object[]
+        {
+            new ClientSave()
+            {
+                Age = 9,
+                Balance = 1,
+                Name = new string('a', 101)
+            }
+        };
     }
 }
