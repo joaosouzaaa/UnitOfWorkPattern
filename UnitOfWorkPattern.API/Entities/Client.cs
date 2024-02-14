dc8e82d3e9f927d0b8c91966243a3e8c1ac7ecc1
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-13 22:24:46 -0300

    Feat: Initial folder structure and entities

---
+++ UnitOfWorkPattern.API/Entities/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitOfWorkPattern.API.Entities;
+
+public sealed class Client
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+    public required decimal Balance { get; set; }
+}
---
+++ UnitOfWorkPattern.API/Entities/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace UnitOfWorkPattern.API.Entities;
+
+public sealed class Log
+{
+    public int Id { get; set; }
+    public required string Message { get; set; }
+    public required DateTime Date { get; set; }
+}
